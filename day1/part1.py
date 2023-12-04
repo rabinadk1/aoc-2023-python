7266008e8f9df9b976718f48262601b8ab69f3c2
Rabin Adhikari <rabin.adk1@gmail.com>
2023-12-04 22:21:45 +0545

    Added input, examples, and source code for day 1

---
+++ day1/part1.py
@@ -0,0 +1,57 @@
+"""
+On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+For example:
+
+```
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+```
+
+In this example, the calibration values of these four lines are 12, 38, 15, and 77.
+Adding these together produces 142.
+
+Consider your entire calibration document.
+What is the sum of all of the calibration values?
+
+Your puzzle answer was 56042.
+"""
+
+def get_sum(inp: str):
+    total_sum = 0
+    inp_list = inp.splitlines()
+
+    for line in inp_list:
+        first_char = None
+        second_char = None
+        for char in line:
+            # Since the text only contains lower case strings or numerals,
+            # checking for lowercase here
+            if not char.islower():
+                if first_char is None:
+                    first_char = char
+                second_char = char
+
+        total_sum += int(first_char + second_char)
+    
+    return total_sum
+
+if __name__ == "__main__":
+    import sys
+
+    args_with_source_code = sys.argv
+
+    if len(args_with_source_code) != 2:
+        # For `python solution.py`, there is one arg.
+        print("Enter a input filename as the next argument.")
+        exit(1)
+    
+    filename = args_with_source_code[1]
+
+    with open(filename) as f:
+        inp = f.read()
+
+    total_sum = get_sum(inp)
+
+    print("The total sum is:", total_sum)
---
+++ day1/part2.py
@@ -0,0 +1,83 @@
+"""
+Your calculation isn't quite right.
+It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line.
+For example:
+
+```
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+```
+
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76.
+Adding these together produces 281.
+
+What is the sum of all of the calibration values?
+
+Your puzzle answer was 55358.
+"""
+
+class DigitTracker:
+    def __init__(self):
+        # Empty initialization
+        self.first_digit = None
+        self.second_digit = None
+
+    def update_dgts(self, digit: int):
+        if self.first_digit is None:
+            self.first_digit = digit
+        # Update both digits even if only one number present
+        self.second_digit = digit
+    
+    def get_sum(self):
+        return self.first_digit * 10 + self.second_digit
+
+
+dgt_literals = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+def get_sum(inp: str):
+    total_sum = 0
+    inp_list = inp.splitlines()
+
+    for line in inp_list:
+        dgt_tracker = DigitTracker()
+        for char_idx, char in enumerate(line):
+            # Since the text only contains lower case strings or numerals,
+            # checking for lowercase here
+            if char.islower():
+                # The literals start with one so starting the lit_idx with 1
+                for dgt_lit_idx, dgt_lit in enumerate(dgt_literals, 1):
+                    if line.startswith(dgt_lit, char_idx):
+                        dgt_tracker.update_dgts(dgt_lit_idx)
+                        break
+            else:
+                dgt_tracker.update_dgts(int(char))
+
+        total_sum += dgt_tracker.get_sum()
+    
+    return total_sum
+
+if __name__ == "__main__":
+    import sys
+
+    args_with_source_code = sys.argv
+
+    if len(args_with_source_code) != 2:
+        # For python solution.py
+        print("Enter a input filename as the next argument.")
+        exit(1)
+    
+    filename = args_with_source_code[1]
+
+    with open(filename) as f:
+        inp = f.read()
+
+    total_sum = get_sum(inp)
+
+    print("The total sum is:", total_sum)
